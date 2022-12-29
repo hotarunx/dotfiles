145dc65d4903b73402a1c7d72ac1aa71b7a597be
hotarunx <29631883+hotarunx@users.noreply.github.com>
2022-12-29 16:01:00 +0900

    add textlnt

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ pip3 install -U -r requirements.txt
 
 echo "Installing n, Node.js, npm and npm packages by n"
 n latest
-npm install -g npm typescript eslint gimonfu npm @vue/cli textlint-filter-rule-comments textlint-rule-ja-space-around-link textlint-rule-preset-ja-spacing textlint-rule-preset-ja-technical-writing textlint-rule-preset-japanese textlint-rule-preset-jtf-style textlint-rule-prh textlint-rule-spellcheck-tech-word textlint
+npm install -g npm typescript eslint gimonfu npm @vue/cli textlint textlint-filter-rule-comments textlint-rule-ja-space-around-link textlint-rule-preset-ja-spacing textlint-rule-preset-ja-technical-writing textlint-rule-preset-japanese textlint-rule-preset-jtf-style textlint-rule-prh textlint-rule-spellcheck-tech-word textlint
 
 echo "dotfiles Installation is finished."
 echo "Please restart terminal."
